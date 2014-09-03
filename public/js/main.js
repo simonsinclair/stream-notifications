c4b4a4357d1a6e51394dd544f440f05aba62dd64
Simon Sinclair <simon@sinclair>
2014-09-03 16:45:39 +0100

    Begin building Stream module

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,28 @@
 // main.js
 //
 
-"use strict";
-
 (function(w, $, undefined) {
+  'use strict';
 
-	function init() {
-		console.log('DOM ready!');
-	}
+	var Stream = {
+    config: {
 
-	$(init);
-})(this, jQuery);
+    },
+    init: function(elemId) {
+      Stream.$elem        = $(elemId);
+      Stream.$settingsBtn = $('#js-stream-settings', Stream.$elem);
+      Stream.$loadCta     = $('#js-stream-load', Stream.$elem);
+      Stream.$alertCta    = $('#js-stream-alert', Stream.$elem);
+
+      Stream.bindEvents();
+    },
+
+    bindEvents: function() {
+      // ...
+    }
+  };
+
+	$(function() {
+    Stream.init('#js-stream');
+  });
+})(window, jQuery);
